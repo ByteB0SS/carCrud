3bf9c4ee37d22aa9b3425b02b68b9b17dfc8d124
ByteB0SS <rubemernesto2@gmail.com>
2025-07-19 08:20:04 +0100

    data-base

---
+++ MySql-DB-Dump20250719/car_project_cars.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE  IF NOT EXISTS `car_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `car_project`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: car_project
+-- ------------------------------------------------------
+-- Server version	8.4.5-0ubuntu0.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `color` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `seating_capacity` int NOT NULL,
+  `fuel_type` enum('Gasolina','Diesel','Flex','Elétrico','Híbrido') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `wheelbase_cm` float NOT NULL,
+  `transmission_type` enum('Manual','Automático','CVT','Semi-automático') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `license_plate` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `acquisition_year` year NOT NULL,
+  `engine_number` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `displacement_cc` int NOT NULL,
+  `chassis_number` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tire_measurements` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `number_of_cylinders` int NOT NULL,
+  `gross_weight_kg` int NOT NULL,
+  `curb_weight_kg` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `license_plate` (`license_plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars`
+--
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-19  8:04:04
