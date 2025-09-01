6bda4cde372bb4e223891574e1f8601b547ef1e3
ByteB0SS <167970963+ByteB0SS@users.noreply.github.com>
2025-09-01 20:22:42 +0100

    Update db.ts

--- BackEnd/api/db.ts
+++ BackEnd/api/db.ts
@@ -4,11 +4,10 @@ import { Pool } from 'pg';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necessário no Neon
+  },
 });
 
-export default pool;
+export default pool;
